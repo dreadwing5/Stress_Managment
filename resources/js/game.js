cb051b3c2912e593b167630c92cc26cfe9eeac77
dreadwing5 <sachinspyk@outlook.com>
2020-10-17 14:10:10 +0530

    Updated Folder Structure

--- resources/js/game.js
+++ resources/js/game.js
@@ -11,7 +11,7 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch('questions.json')
+fetch('/resources/static/questions.json')
     .then((res) => {
         return res.json();
     })
@@ -38,7 +38,7 @@ getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
-        return window.location.assign('/end.html');
+        return window.location.assign('/pages/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
